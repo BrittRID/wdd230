45b430e09cdf6e6aecccb181cf0606fc9fb739fa
Brittany Ridley <britt.ridley@outlook.com>
2023-07-08 14:52:19 -0500

    first commit

---
+++ scripts/counter.js
@@ -0,0 +1,11 @@
+// Retrieve the visitor count from local storage or initialize it to 0
+let visitorCount = localStorage.getItem("visitorCount") || 0;
+
+// Increment the visitor count by 1
+visitorCount++;
+
+// Update the visitor count on the page
+document.getElementById("visitorCount").textContent = visitorCount;
+
+// Store the updated visitor count in local storage
+localStorage.setItem("visitorCount", visitorCount);
