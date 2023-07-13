d4a6cf349f60422439fead041a6c9aa53b11a10b
Brittany Ridley <britt.ridley@outlook.com>
2023-07-12 19:31:13 -0500

    new file

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,14 @@
 const baseURL = "https://brittrid.github.io/wdd230/";
 const linksURL = "https://brittrid.github.io/wdd230/data/links.json";
+
+const apiKey = "YOUR_API_KEY";
+const location = "YOUR_LOCATION";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // Process the weather data here
+  })
+  .catch(error => {
+    console.log("Error fetching weather data:", error);
+  });
