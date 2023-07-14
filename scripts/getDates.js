5bbf7de7feababe48c2a3c275bfb254bb5a287f0
Brittany Ridley <britt.ridley@outlook.com>
2023-07-13 22:23:59 -0500

    css for discover

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,16 @@ document.getElementById('currentyear').textContent = new Date().toLocaleDateStri
 
 let oLastModif = new Date(document.lastModified);
 document.getElementById('lastModified').textContent = oLastModif;
+
+
+function calculateDays() {
+    var startDate = new Date(document.getElementById('startDate').value);
+    var endDate = new Date(document.getElementById('endDate').value);
+
+    // Calculate the difference in milliseconds
+    var diffInMs = Math.abs(endDate - startDate);
+
+    // Convert milliseconds to days
+    var diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+    document.getElementById('results').innerHTML = "Days between the dates: " + diffInDays;}
